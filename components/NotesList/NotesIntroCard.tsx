f80f4c6180ecd23da18da4a5ad387a1524e12739
Saif <shaiksaifuddin95@gmail.com>
2022-09-20 00:55:09 +0530

    completed basic setup of notes component and testing page

---
+++ components/NotesList/NotesIntroCard.tsx
@@ -0,0 +1,5 @@
+function NotesIntroCard() {
+    return ( <h1>This is notes intro card</h1> );
+}
+
+export default NotesIntroCard;
---
+++ pages/test.tsx
@@ -0,0 +1,11 @@
+import NotesIntroCard from "../components/NotesList/NotesIntroCard";
+
+function Test() {
+    return <div>
+        <h1>This is a test page</h1>
+        <p>Test the individual components here</p>
+        <NotesIntroCard></NotesIntroCard>
+    </div>
+}
+
+export default Test;
